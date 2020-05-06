cf5f0cef1cda9ff9af6ea3d1c8fca9d89e6d4790
duhow <contacto@duhowpi.net>
2020-05-06 16:48:36 +0200

    Add Lo-fi Hip hop

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,3 @@
 alias please='sudo'
 alias dhfm='nohup vlc --one-instance http://digitalhits.frilab.com:8000/digitalhits </dev/null &>/dev/null &'
+alias lofi='nohup vlc --one-instance --no-video https://www.youtube.com/watch\?v\=5qap5aO4i9A </dev/null &>/dev/null &'
