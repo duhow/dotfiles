5d10283141b574da7ac6ca420e70882485ca530b
duhow <contacto@duhowpi.net>
2020-11-10 12:19:35 +0100

    Add df alias for hiding snap lists

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,6 @@
 alias please='sudo'
 alias dhfm='nohup vlc --one-instance http://digitalhits.frilab.com:8000/digitalhits </dev/null &>/dev/null &'
 alias lofi='nohup vlc --one-instance --no-video https://www.youtube.com/watch\?v\=5qap5aO4i9A </dev/null &>/dev/null &'
+
+# hide snap
+alias df='df -x"squashfs"'
